167453f6e5b738150944eedb6dd298c7b72e6624
ealvesjr90 <ealvesjr90@gmail.com>
2026-03-05 20:22:20 -0300

    net1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
+module "networking" {
+  source         = "./modules/networking"
+  compartment_id = var.compartment_id
+}
+
 module "ocir" {
   source         = "./modules/ocir"
+  compartment_id = var.compartment_id
+}
+
+module "nosql" {
+  source         = "./modules/nosql"
   compartment_id = var.compartment_id
 }
