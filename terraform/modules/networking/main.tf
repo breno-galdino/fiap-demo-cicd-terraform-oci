8c68a92c8fdef5abd4adf14a3ef0902cea7c9617
ealvesjr90 <ealvesjr90@gmail.com>
2026-03-08 18:46:14 -0300

    network

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -4,16 +4,75 @@ resource "oci_core_vcn" "main" {
   display_name   = "togglemaster-vcn"
 }
 
+# Internet Gateway
+resource "oci_core_internet_gateway" "igw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "togglemaster-igw"
+  enabled        = true
+}
+
+# Route Table para acesso à internet
+resource "oci_core_route_table" "public_rt" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "public-route-table"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.igw.id
+  }
+}
+
+# Security List liberando SSH
+resource "oci_core_security_list" "public_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "public-security-list"
+
+  ingress_security_rules {
+
+    protocol = "6" # TCP
+
+    source = "0.0.0.0/0"
+
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+}
+
+# Subnet Workers
 resource "oci_core_subnet" "workers" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.main.id
   cidr_block     = "10.0.3.0/24"
   display_name   = "workers-subnet"
+
+  route_table_id = oci_core_route_table.public_rt.id
+
+  security_list_ids = [
+    oci_core_security_list.public_sl.id
+  ]
 }
 
+# Subnet DB
 resource "oci_core_subnet" "db" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.main.id
   cidr_block     = "10.0.5.0/24"
   display_name   = "db-subnet"
+
+  route_table_id = oci_core_route_table.public_rt.id
+
+  security_list_ids = [
+    oci_core_security_list.public_sl.id
+  ]
 }
