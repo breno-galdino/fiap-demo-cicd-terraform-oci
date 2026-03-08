cfd83226ef544525418db9b1af6eadd13d4c2889
ealvesjr90 <ealvesjr90@gmail.com>
2026-03-08 17:51:34 -0300

    postgres 5

--- terraform/modules/postgres/main.tf
+++ terraform/modules/postgres/main.tf
@@ -1,8 +1,21 @@
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+data "oci_core_images" "oracle_linux_arm" {
+  compartment_id           = var.compartment_id
+  operating_system         = "Oracle Linux"
+  operating_system_version = "9"
+  shape                    = "VM.Standard.A1.Flex"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
 resource "oci_core_instance" "postgres" {
 
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_id
-  display_name        = "postgres-instance"
+  display_name        = "postgres-vm"
 
   shape = "VM.Standard.A1.Flex"
 
@@ -18,11 +31,12 @@ resource "oci_core_instance" "postgres" {
 
   source_details {
     source_type = "image"
-    source_id   = var.image_id
+    source_id   = data.oci_core_images.oracle_linux_arm.images[0].id
   }
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
+    user_data           = base64encode(file("${path.module}/cloud-init.yaml"))
   }
 }
 
